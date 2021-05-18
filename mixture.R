54c381c01b84d9ec8d78003964ab7a1b1ff828c6
Qi <owenqiz@users.noreply.github.com>
2021-05-18 00:39:08 -0400

    Add files via upload

---
+++ mixture.R
@@ -0,0 +1,26 @@
+p <- c(.75, .15, .1)
+
+mix1 <- function(n, prob){
+  x <- rep(NA, n)
+  for(i in 1:n){
+    u <- runif(1)
+    if (u < prob[1])
+      x[i] <- rt(1, 4)
+    else if (u < prob[2])
+      x[i] <- rexp(1, 1/2)
+    else
+      x[i] <- runif(1, -3, 3)
+  }
+  return(x)
+}
+
+x <- mix1(5000, p)
+
+mix2 <- function(n, prob){
+  x <- rep(NA, n)
+  idx <- sample(1:3, size = n, prob = prob, replace = TRUE)
+  x[idx == 1] <- rt(length(x[idx == 1]), 4)
+  x[idx == 2] <- rexp(length(x[idx == 2]), 2)
+  x[idx == 3] <- runif(length(x[idx == 3]), -3, 3)
+  return(x)
+}
